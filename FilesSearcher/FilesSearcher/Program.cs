0b163ae8e738c8edd9655aab8a7a33ce513321ef
SvirXII <svirxii@yandex.ru>
2018-03-31 21:10:56 +0300

    Added validation checks. Corrected spelling mistake.

--- FilesSearcher/FilesSearcher/Program.cs
+++ FilesSearcher/FilesSearcher/Program.cs
@@ -13,60 +13,87 @@ namespace FilesSearcher
         {
             Console.WriteLine("Please enter directory");
             string directory = Console.ReadLine();
-            Console.WriteLine("Pleas enter file extension");
+            Console.WriteLine("Please enter file extension");
             string extension = Console.ReadLine();
             string path, ext;
 
-            //need to replace "C:/" and "sys" by readline
-            path = @directory;
-            ext = "*." + extension;
-
-            //searching all files info in selected directory with selected extension
-            try
+            if(directory != "" && extension != "")
             {
-                FileSystemInfo[] files = new DirectoryInfo(path).GetFileSystemInfos(ext);
-                DateTime time = new DateTime(1900, 1, 1, 1, 1, 1, 1);
-                DateTime x = time;
-                FileSystemInfo LastCreatedFile = files[0];
+                path = @directory;
+                ext = "*." + extension;
 
-                //selecting last created file
-                foreach (FileSystemInfo file in files)
+                //searching all files info in selected directory with selected extension
+                try
                 {
-                    if (x < file.CreationTime)
-                    {
-                        x = file.CreationTime;
-                        LastCreatedFile = file;
-                    }
-                }
+                    FileSystemInfo[] files = new DirectoryInfo(path).GetFileSystemInfos(ext);
+                    DateTime time = new DateTime(1900, 1, 1, 1, 1, 1, 1);
+                    DateTime x = time;
+                    FileSystemInfo LastCreatedFile = files[0];
 
-                Console.WriteLine("Last created file:");
-                Console.WriteLine("{0} was created - {1}", LastCreatedFile.Name, LastCreatedFile.CreationTime);
-                Console.WriteLine();
-                Console.WriteLine("Recently created files:");
-
-                int count = 1;
-                for (int i = 0; i < files.Length; i++)
-                {
-                    //exclude last created file
-                    if (files[i].Name != LastCreatedFile.Name)
+                    if (files.Length != 0)
                     {
-                        //decrease creation date of the last created file by 10 seconds 
-                        //and compare it to all files in massive
-                        if (LastCreatedFile.CreationTime.AddSeconds(-10) <= files[i].CreationTime)
+                        //selecting last created file
+                        foreach (FileSystemInfo file in files)
                         {
-                            Console.WriteLine("{0}. {1} was created - {2}", count, files[i].Name, files[i].CreationTime);
-                            count++;
+                            if (x < file.CreationTime)
+                            {
+                                x = file.CreationTime;
+                                LastCreatedFile = file;
+                            }
+                        }
+
+                        Console.WriteLine("Last created file:");
+                        Console.WriteLine("{0} was created - {1}", LastCreatedFile.Name, LastCreatedFile.CreationTime);
+                        Console.WriteLine();
+                        Console.WriteLine("Recently created files:");
+
+                        int count = 1;
+                        for (int i = 0; i < files.Length; i++)
+                        {
+                            if (i != 0)
+                            {
+                                //exclude last created file
+                                if (files[i].Name != LastCreatedFile.Name)
+                                {
+                                    //decrease creation date of the last created file by 10 seconds 
+                                    //and compare it to all files in massive
+                                    if (LastCreatedFile.CreationTime.AddSeconds(-10) <= files[i].CreationTime)
+                                    {
+                                        Console.WriteLine("{0}. {1} was created - {2}", count, files[i].Name, files[i].CreationTime);
+                                        count++;
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("There is no recent files");
+                            }
                         }
                     }
+                    else
+                    {
+                        Console.WriteLine("There is no files with selected exstension in the selected directory");
+                    }
+                }
+                catch (System.IO.DirectoryNotFoundException)
+                {
+                    Console.WriteLine("*********************************");
+                    Console.WriteLine("Entered directory doesn't exist");
+                    Console.WriteLine("*********************************");
+                }
+                catch (System.ArgumentException)
+                {
+                    Console.WriteLine("*********************************");
+                    Console.WriteLine("Entered directory doesn't exist");
+                    Console.WriteLine("*********************************");
                 }
             }
-
-            catch (System.IO.DirectoryNotFoundException)
+            else
             {
-                Console.WriteLine("*********************************");
-                Console.WriteLine("Entered directory doesn't exist");
-                Console.WriteLine("*********************************");
+                Console.WriteLine("Searching directory or/and file extension was not entered");
             }
+
+            Console.ReadLine();
         }
     } 
 }
